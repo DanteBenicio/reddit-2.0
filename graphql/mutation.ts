6059f0d661af736cbeeadf7131f074fb8c069b8d
Dante Benicio <dantecosta79@gmail.com>
2022-05-24 21:47:17 -0300

    refactor

--- graphql/mutation.ts
+++ graphql/mutation.ts
@@ -1,8 +1,20 @@
 import { gql } from '@apollo/client'
 
 export const ADD_POST = gql`
-  mutation MyMutation($body: String!, $image: String!, $subreddit_id: ID!, $title: String!, $username: String!) {
-    insertPost(body: $body, image: $image, subreddit_id: $subreddit_id, title: $title, username: $username) {
+  mutation MyMutation(
+    $body: String!
+    $image: String!
+    $subreddit_id: ID!
+    $title: String!
+    $username: String!
+  ) {
+    insertPost(
+      body: $body
+      image: $image
+      subreddit_id: $subreddit_id
+      title: $title
+      username: $username
+    ) {
       body
       created_at
       id
