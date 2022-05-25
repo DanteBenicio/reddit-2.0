1db7b3e4ad28bfbd306bb06dfc1618b4202c747c
Dante Benicio <dantecosta79@gmail.com>
2022-05-24 21:46:33 -0300

    add media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -5,6 +6,23 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'mb': {'max': '420px'},
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [],
 }
