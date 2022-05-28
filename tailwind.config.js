d00c9b4036a3089b0559d20678b2e0ba885d58b3
Dante Benicio <dantecosta79@gmail.com>
2022-05-27 22:14:08 -0300

    set colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: {
+          600: '#161617',
+          700: '#1A1A1B',
+          800: '#030303'
+        },
+        gray: {
+          300: '#C8CBCD',
+          500: '#818384',
+          800: '#353537',
+          850: '#393A3B',
+          900: '#232324'
+        },
+        orange: {
+          500: '#FF4500'
+        }
+      }
+    },
     screens: {
       'mb': {'max': '420px'},
       'sm': '640px',
