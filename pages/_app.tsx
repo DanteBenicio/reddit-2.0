dc2d9dab9f6d266d0a9edc00be141cc8e5eada59
Dante Benicio <dantecosta79@gmail.com>
2022-06-13 22:39:10 -0300

    render ContextProvider and SidebarMenu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,16 +5,19 @@ import Header from '../components/Header'
 import { ApolloProvider } from '@apollo/client'
 import { client } from '../apollo-client'
 import { Toaster } from 'react-hot-toast'
+import { ContextProvider } from '../context'
+import SidebarMenu from '../components/SidebarMenu'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <SessionProvider session={session}>
         <Toaster />
-        <>
+        <ContextProvider>
+          <SidebarMenu />
           <Header />
           <Component {...pageProps} />
-        </>
+        </ContextProvider>
       </SessionProvider>
     </ApolloProvider>
   )
